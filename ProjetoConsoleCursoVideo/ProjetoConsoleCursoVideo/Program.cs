2abb7f634fab499ffb7c685da292d2eb4e9710da
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-14 16:31:46 -0300

    Aprendi gerar números aleatórios com random

--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
@@ -1,20 +1,11 @@
 ﻿
-double w = -5;
-double x = 3;
-double y = 81;
-double z = 4.2;
 
-double a = Math.Pow(x, 3);
-double b = Math.Sqrt(y);
-double c = Math.Abs(w);
-double d = Math.Round(z);
-double e = Math.Ceiling(z);
-double f = Math.Floor(z);
-double g = Math.Max(y, x);
-double h = Math.Min(y, x);
+Random random = new Random();
 
-Console.WriteLine(h);
+int numero = random.Next(1,70000);
+double numeroDecimal= random.NextDouble();
 
+Console.WriteLine(numeroDecimal);
 
 Console.ReadKey();
 
