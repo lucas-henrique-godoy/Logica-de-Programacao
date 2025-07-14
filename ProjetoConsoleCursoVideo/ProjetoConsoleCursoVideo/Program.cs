1e6bbd796c65ea1b96e50e70f4edc8b7214443ab
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-13 22:44:23 -0300

    Aprendi sobre conversões de tipos de dados e como pegar as informações do usuario.

--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
@@ -1,8 +1,15 @@
 ﻿
 
-const double pi = 3.14159;
+Console.WriteLine("Qual é o seu nome?");
+string nome = Console.ReadLine();
 
 
+
+Console.WriteLine("Qual a dua idade?");
+ int idade = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"O seu nome é: {nome} e a sua idade é: {idade}");
 
 
 
--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c181de9be89a97dca5e9d0c46f344e49bf7b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7010152047873e52799cd49b1f3a93ff72dd81a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
