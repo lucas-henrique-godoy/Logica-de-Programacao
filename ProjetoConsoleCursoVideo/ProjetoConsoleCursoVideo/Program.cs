1d58f0ec30d833ea82dd37b7685cdacfc50d17ce
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-15 16:55:07 -0300

    Irei aprender sobre o loop for

--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
@@ -1,21 +1,14 @@
 ﻿
 /*
 
-    while lop
+    for lop
  
  */
 
-Console.WriteLine("Qual seu nome?");
-string nome = Console.ReadLine();
 
 
-while (nome == "")
-{
-    Console.WriteLine("Qual seu nome?");
-    nome = Console.ReadLine();
-}
 
-Console.WriteLine("Olá " + nome);
+
 
 
 
--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f274f277f390af12ed6ba9ca7faee9e01a9a274")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cee8f51a59164c4cc47ed28fde017dcdff8dbd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
