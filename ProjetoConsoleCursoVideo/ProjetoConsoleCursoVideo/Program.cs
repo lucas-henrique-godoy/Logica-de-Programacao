d706ec81fbd261cda3822d325044acf5457e4208
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-16 18:01:44 -0300

    Update Program.cs

--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
@@ -1,31 +1,6 @@
 ﻿
 
 
-/*
-    nested loops
- 
- */
-
-Console.Write("Quantas linhas?: ");
-int linhas = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Quantas colunas ?: ");
-int colunas = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Qua o símbolo?: ");
-string simbolo = Console.ReadLine();
-
-
-for (int i = 0; i < linhas; i++)
-{
-    for(int j= 0; j < colunas; j++)
-    {
-        Console.Write(simbolo);
-    }
-
-    Console.WriteLine();
-}
-
 
 
 
