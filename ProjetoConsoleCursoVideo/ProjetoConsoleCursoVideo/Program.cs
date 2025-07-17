9b78466d3ed9052241bcbca725cfcd1e46f64efc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-16 22:58:28 -0300

    Estou aprendendo sobre arrays

--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
@@ -1,63 +1,16 @@
 ﻿
-Random randon = new Random();
+//ARRAY
 
-bool jogarNovamente = true;
-int min = 1;
-int max = 100;
-int chute;
-int numero;
-int tentativas;
-string resposta;
 
-while(jogarNovamente)
-{
-    chute = 0;
-    tentativas = 0;
-    numero = randon.Next(min, max + 1);
-    resposta = "";
+string[] frutas = { "Laranja", "Uva", "Limão", "Kiwi" };
 
-    while (chute != numero)
-    {
-        Console.WriteLine($"Adivinhe o número entre {min} e {max}: ");
-        chute = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Chute: {chute}");
+Console.WriteLine(frutas[0]);
 
-        if (chute > numero)
-        {
-            Console.WriteLine($"O número é menor que {chute}");
-        }
 
-        if (chute < numero)
-        {
-            Console.WriteLine($"O número é maior que {chute}");
-        }
 
-        tentativas++;
-    }
 
-    Console.WriteLine($"Número: {numero}");
-    Console.WriteLine("Você venceu!");
-    Console.WriteLine($"Tentativas:  {tentativas}");
 
-    Console.WriteLine("Você gostaria de jogar novamente? (S/N): ");
-    resposta = Console.ReadLine();
-    resposta = resposta.ToUpper();
 
-    if (resposta == "S")
-    {
-        jogarNovamente = true;
-    }
-    else if (resposta == "N") 
-    {
-        jogarNovamente = false;
-    }
-    else
-    {
-        Console.WriteLine("Você colocou uma opção errada,  digite S ou N");
-    }
-}
-
-Console.WriteLine("Obrigado por jogar!");
 
 
 
@@ -65,25 +18,3 @@ Console.WriteLine("Obrigado por jogar!");
 
 
 Console.ReadKey();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d706ec81fbd261cda3822d325044acf5457e4208")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f3ac3653f79923793d8d7d02d1017c5a7f9e8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
