2955f7e57a5a753f72cfc84f3c5668f4da7e8ba5
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-17 18:01:08 -0300

    Update Program.cs

--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
@@ -1,49 +1,16 @@
 ﻿
 
-//MÉTODOS COM PARÂMETROS
-
-double num1;
-double num2;
-
-Console.Write("Digite o primeiro número: ");
-num1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Digite o segundo número: ");
-num2 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Qual operação? (* ou /): ");
-string operacao = Console.ReadLine();
+//Params keyword = parametro de um  método que recebe uma variávelnumero de parametros, o tipo de parametro deve ser unico
 
 
-if (operacao == "*")
-{
-    double result = Multiplicar(num1, num2);
-    Console.WriteLine($"O resultado da multiplicação entre {num1} e {num2} é:  {result}");
-}
-else
-{
-    double result = Divisao(num1, num2);
-    Console.WriteLine($"O resultado da divisão entre {num1} e {num2} é:  {result}");
-}
+
+
 
 
     
 
 
 
-
-static double Multiplicar(double num1, double num2)
-{
-  double resultado =  num1* num2;
-  return resultado;
-}
-
-
-static double Divisao(double num1, double num2)
-{
-    double resultado = num1 / num2;
-    return resultado;
-}
 
 
 Console.ReadKey();
