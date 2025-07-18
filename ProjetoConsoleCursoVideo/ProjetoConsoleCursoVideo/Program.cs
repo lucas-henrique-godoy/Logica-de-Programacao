0c758d3b83bf5afb5e69d5f2459cb114ef1d2754
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-17 22:11:02 -0300

    Aprendi sobre o operador ternário que é como se fosse um if/else mais simples.

--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/Program.cs
@@ -1,35 +1,15 @@
 ﻿
 
-//  TRY CATCH FINALLY
+//  Operador Ternário = O operadr ternário é uma forma curta e simples de escrever um if/else em uma única linha.
+
+Console.WriteLine("Qual sua idade?: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+string mensagem = idade >= 18 ? "Você é de maior!" : "Você é de menor!";
+Console.WriteLine(mensagem);
 
 
-int num1;
-int num2;
-int num3;
-int resultado;
 
-
-try
-{
-    Console.WriteLine("Digite o primeiro número: ");
-    num1 = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Digite o segundo número: ");
-    num2 = Convert.ToInt32(Console.ReadLine());
-
-
-    resultado = num1 / num2;
-
-    Console.WriteLine($"  {num1} / {num2} = {resultado} ");
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-finally
-{
-    Console.WriteLine("Fim!");
-}
 
 
 
--- ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
+++ ProjetoConsoleCursoVideo/ProjetoConsoleCursoVideo/obj/Debug/net9.0/ProjetoConsoleCursoVideo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2496a870ae40233703571b894c0599362ff4739a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a67211bdd5ed6c4c4261fb55733741b959d2e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoConsoleCursoVideo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
