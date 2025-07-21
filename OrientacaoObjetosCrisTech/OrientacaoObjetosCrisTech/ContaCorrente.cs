933c0e2f4fca8d20547ec5c79315f6e59cee2905
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-20 22:33:58 -0300

    Estou testando o funcionamento da herança no C#

--- OrientacaoObjetosCrisTech/OrientacaoObjetosCrisTech/ContaCorrente.cs
+++ OrientacaoObjetosCrisTech/OrientacaoObjetosCrisTech/ContaCorrente.cs
@@ -8,6 +8,28 @@ namespace OrientacaoObjetosCrisTech
 {
     class ContaCorrente : ContaBancaria
     {
+        public double LimiteChequeEspecial { get; set; }
 
+        public void PodeSacar(double valor)
+        {
+            var podeSacar = Saldo + LimiteChequeEspecial >= valor;
+
+            if (podeSacar) 
+            {
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("É possível sacar o valor solicitado: ");
+                Console.WriteLine($"Valor  solicitado: R${valor}");
+                Console.WriteLine($"Saldo + cheque especial: R${Saldo + LimiteChequeEspecial}   ");
+                Console.WriteLine("--------------------------");
+            }
+            else
+            {
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Não é possível sacar o valor solicitado: ");
+                Console.WriteLine($"Valor  solicitado: R${valor}");
+                Console.WriteLine($"Saldo + cheque especial: R${Saldo + LimiteChequeEspecial}   ");
+                Console.WriteLine("--------------------------");
+            }
+        }
     }
 }
--- OrientacaoObjetosCrisTech/OrientacaoObjetosCrisTech/Program.cs
+++ OrientacaoObjetosCrisTech/OrientacaoObjetosCrisTech/Program.cs
@@ -8,12 +8,4 @@ cliente.Nome = "Lucas";
 cliente.CPF = "123456789";
 
 
-ContaBancaria conta = new ContaBancaria();
-
-
-
-conta.Titular = cliente;
-conta.NumeroConta = "123456";
-conta.Agencia = "0001";
-
-conta.MostrarDados();
+Console.ReadKey();
