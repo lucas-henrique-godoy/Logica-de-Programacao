caf9496c9d6642f1763b8d46b7046ab3461b35f2
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-23 16:10:54 -0300

    Criei a pasta de services que será responsável pela lógica de mostrar os menus, e fazer as operações com livros e usuários, etc.

--- SistemaEmprestimosCrisTech/SistemaEmprestimosCrisTech/obj/Debug/net9.0/SistemaEmprestimosCrisTech.AssemblyInfo.cs
+++ SistemaEmprestimosCrisTech/SistemaEmprestimosCrisTech/obj/Debug/net9.0/SistemaEmprestimosCrisTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEmprestimosCrisTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3e2444076f0e331dcfb7af34b2047fe583b741")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEmprestimosCrisTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaEmprestimosCrisTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
