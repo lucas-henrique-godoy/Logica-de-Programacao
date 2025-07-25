9ed09e37a610db83c5929626524345c7ca681375
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-07-24 22:42:57 -0300

    Criei e testei todos os métodos crud de usuários.

--- SistemaEmprestimosCrisTech/SistemaEmprestimosCrisTech/Services/BibliotecaService.cs
+++ SistemaEmprestimosCrisTech/SistemaEmprestimosCrisTech/Services/BibliotecaService.cs
@@ -177,12 +177,12 @@ namespace SistemaEmprestimosCrisTech.Services
             string autor = Console.ReadLine();
 
             livros.Add(new Livro { Id = livroIdCounter ++, Titulo = titulo, Autor = autor});
-            Console.WriteLine("Livro Adicionado com sucesso!");
+            Console.WriteLine("Livro adicionado com sucesso!");
         }
 
         private void ListarLivros()
         {
-            Console.WriteLine("\nLista de Livros:");
+            Console.WriteLine("\nLista de Livros: ");
 
             foreach (Livro livro in livros)
             {
@@ -220,11 +220,11 @@ namespace SistemaEmprestimosCrisTech.Services
             int id = Convert.ToInt32(Console.ReadLine());
 
             livros.RemoveAll(l => l.Id == id);
-            Console.WriteLine("Livro Removido!");
+            Console.WriteLine("Livro removido!");
         }
 
-        //Métodos Usuários
 
+        //Métodos Usuários
         private void AdicionarUsuario()
         {
             Console.Write("Nome: ");
@@ -239,18 +239,45 @@ namespace SistemaEmprestimosCrisTech.Services
 
         private void ListarUsuarios()
         {
+            Console.WriteLine("\nLista de Usuários: ");
 
+            foreach (var usuario in usuarios)
+            {
+                Console.WriteLine($"ID: {usuario.Id} | Nome: {usuario.Nome} | Email: {usuario.Email}");
+            }
         }
 
         private void AtualizarUsuario()
         {
+            Console.WriteLine("Id do usuário a atualizar: ");
+            int id = Convert.ToInt32(Console.ReadLine());
 
+            Usuario usuario = usuarios.FirstOrDefault(u => u.Id == id);
+            
+            if (usuario != null)
+            {
+                Console.Write("Novo Nome: ");
+                usuario.Nome = Console.ReadLine();
+
+                Console.Write("Novo Email: ");
+                usuario.Email = Console.ReadLine();
+
+                Console.WriteLine("Usuário atualizado!");
+            }
+            else
+            {
+                Console.WriteLine("Usuário não encontrado!");
+            }
         }
 
 
         private void RemoverUsuario() 
-        { 
-        
+        {
+            Console.WriteLine("ID do usuário a remover: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            usuarios.RemoveAll(u => u.Id == id);
+            Console.WriteLine("Usuário removido!");
         }
 
      }
--- SistemaEmprestimosCrisTech/SistemaEmprestimosCrisTech/obj/Debug/net9.0/SistemaEmprestimosCrisTech.AssemblyInfo.cs
+++ SistemaEmprestimosCrisTech/SistemaEmprestimosCrisTech/obj/Debug/net9.0/SistemaEmprestimosCrisTech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEmprestimosCrisTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc52169945154fa4a8f8c42fc7ef53949e9853a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce3f21cea5263878eae5fb72c2f889c0e0e5775")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEmprestimosCrisTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaEmprestimosCrisTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
